cd46f9b8977f241b93c8f15dc0d0308ec3c0bdc3
KevinAGV <47923484+KevinAGV@users.noreply.github.com>
2019-04-12 18:07:37 -0500

    Clase de producto y formulario

---
+++ Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket
+{
+    class Product
+    {
+        private int _cantidad;
+        private double _costo;
+        private double _total;
+        private string _nombre;
+
+        public int cantidad
+        {
+            get { return _cantidad; }
+        }
+
+        public double costo
+        {
+            get { return _costo; }
+        }
+
+        public double total
+        {
+            get { return _total; }
+        }
+
+      
+
+        public Product (int cantidad, double costo, double total, string nombre)
+        {
+            _cantidad = cantidad;
+            _costo = costo;
+            _total = total;
+            _nombre = nombre;
+        }
+
+        public double importe(int cantidad,double costo)
+        {
+            double importe = _cantidad * _costo;
+            return importe;
+
+        }
+
+        public double iva(double subtotal)
+        {
+            double iva = subtotal * 0.16;
+            return iva;
+        }
+
+        
+    }
+}
